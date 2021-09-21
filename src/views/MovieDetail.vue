8745eadfae5f900f42a56a52120d0595362a4bcb
Jyo561 <jyotiradityakuanar34@gmail.com>
2021-09-21 12:47:39 +0530

    changes

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -3,6 +3,9 @@
 		<h2>{{ movie.Title }}</h2>
 		<p>{{ movie.Year }}</p>
 		<img :src="movie.Poster" alt="Movie Poster" class="featured-img" />
+		<br>
+		<h4 class="linew">Rated:{{ movie.Rated }}</h4>
+		<h3 class="linew" style="margin-left:20px">Rating:{{ movie.imdbRating }}</h3>
 		<p>{{ movie.Plot }}</p>
 	</div>
 </template>
@@ -18,7 +21,7 @@ export default {
 		const route =useRoute();
 
 		onBeforeMount(() => {
-				fetch(`https://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
+				fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`)
 					.then(response => response.json())
 					.then(data => {
 						movie.value = data;
@@ -35,7 +38,7 @@ export default {
 <style lang="scss">
 .movie-detail{
 	padding: 16px;
-
+  align-items: center;
 	h2{
 		color: #FFF;
 		font-size: 28px;
@@ -46,6 +49,11 @@ export default {
 		color: #FFF;
 		font-size: 16px;
 		line-height: 1.4;
+	}
+
+	.linew{
+		display: inline;
+		color: #FFF;
 	}
 }
 </style>
